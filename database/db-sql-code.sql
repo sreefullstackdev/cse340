966c6be6f6d19ab95b833b7066295c9e86d86f32
sreefullstackdev <skoonisetti@byupathway.edu>
2025-09-13 11:21:19 +0530

    minor change

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340database;
+    OWNER TO cse340_dbase;
 
 -- Table structure for table 'classification'
 CREATE TABLE public.classification (
